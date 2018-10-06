7462e47eefd72a53a6503f195c1440c12f810ee4
gepres <genaropretill@gmail.com>
2018-10-06 15:43:32 -0500

    media query de table

--- js/styles.js
+++ js/styles.js
@@ -1,23 +1,58 @@
-const masonryLayout = (containerElem, itemsElems, columns) => {
-  containerElem.classList.add('masonry-layout', `columns-${columns}`)
-  let columnsElements = []
+const web = matchMedia('(min-width: 752px)');
+const changeSize2 = mq => {
+  if(mq.matches === true){
+    const masonryLayout = (containerElem, itemsElems, columns) => {
+      containerElem.classList.add('masonry-layout', `columns-${columns}`)
+      let columnsElements = []
 
-  for (let i = 1; i <= columns; i++) {
-    let column = document.createElement('div')
-    column.classList.add('masonry-column', `column-${i}`)
-    containerElem.appendChild(column)
-    columnsElements.push(column)
-  }
+      for (let i = 1; i <= columns; i++) {
+        let column = document.createElement('div')
+        column.classList.add('masonry-column', `column-${i}`)
+        containerElem.appendChild(column)
+        columnsElements.push(column)
+      }
 
-  for (let m = 0; m < Math.ceil(itemsElems.length / columns); m++) {
-    for (let n = 0; n < columns; n++) {
-      let item = itemsElems[m * columns + n]
-      columnsElements[n].appendChild(item)
-      item.classList.add('masonry-item')
+      for (let m = 0; m < Math.ceil(itemsElems.length / columns); m++) {
+        for (let n = 0; n < columns; n++) {
+          let item = itemsElems[m * columns + n]
+          columnsElements[n].appendChild(item)
+          item.classList.add('masonry-item')
+        }
+      }
     }
+    masonryLayout(document.getElementById('gallery'), document.querySelectorAll('.gallery-item'), 3);
   }
-}
-masonryLayout(document.getElementById('gallery'), document.querySelectorAll('.gallery-item'), 3);
+  }
+web.addListener(changeSize2);
+changeSize2(web);
+
+const table = matchMedia('(max-width: 750px)');
+const changeSize = mq => {
+  if (mq.matches === true) {
+    const masonryLayout = (containerElem, itemsElems, columns) => {
+      containerElem.classList.add('masonry-layout', `columns-${columns}`)
+      let columnsElements = []
+
+      for (let i = 1; i <= columns; i++) {
+        let column = document.createElement('div')
+        column.classList.add('masonry-column', `column-${i}`)
+        containerElem.appendChild(column)
+        columnsElements.push(column)
+      }
+
+      for (let m = 0; m < Math.ceil(itemsElems.length / columns); m++) {
+        for (let n = 0; n < columns; n++) {
+          let item = itemsElems[m * columns + n]
+          columnsElements[n].appendChild(item)
+          item.classList.add('masonry-item')
+        }
+      }
+    }
+    masonryLayout(document.getElementById('gallery'), document.querySelectorAll('.gallery-item'), 2);
+   }
+ }
+ table.addListener(changeSize);
+ changeSize(table);
 
 
 // carousel
